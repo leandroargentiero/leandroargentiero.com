a264a036ede41a1d394356fdf35a6ee1102da586
Leandro Argentiero <argentiero.leo@gmail.com>
2022-04-06 16:12:43 +0200

    Added fonts and extended theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["Space Grotesk", "serif"],
+        body: ["IBM Plex Sans", "sans-serif"],
+      },
+      colors: {
+        "pastel-pink": "#F2E3EE",
+        "pastel-indigo": "#9FB0EA",
+        "pastel-blue": "#B0CDEB",
+      },
+    },
   },
   plugins: [],
 };
