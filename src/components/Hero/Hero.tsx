ec57877907f073c31f4696bfaf3da7fe8b951306
Leandro Argentiero <argentiero.leo@gmail.com>
2022-04-07 19:47:51 +0200

    Different Hero height on smaller screens

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -30,7 +30,7 @@ export const Hero = () => {
         initial="initial"
         animate="end"
         variants={containerBorderRadius}
-        className="relative h-full w-full overflow-hidden rounded-3xl bg-pastel-pink"
+        className="relative h-[85vh] w-full overflow-hidden rounded-3xl bg-pastel-pink md:h-full"
       >
         <div className="absolute top-0 left-0 z-20 h-full w-full">
           <motion.header
