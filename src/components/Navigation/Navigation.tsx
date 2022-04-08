508c7c912c99e1ddbc07b4e21124a23fc05a5a32
Leandro Argentiero <argentiero.leo@gmail.com>
2022-04-08 17:18:18 +0200

    Added animations

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,14 +1,27 @@
+import { motion } from "framer-motion";
+
 import { NavItem } from "./NavItem";
+import { navContainer, navList } from "./navigationVariants";
 
 export const Navigation = () => (
-  <nav className="fixed bottom-8 left-1/2 translate-x-[-50%] z-20">
-    <ul className="flex gap-2 flex-row justify-center items-center py-2 px-4 rounded-full backdrop-blur-[5px] bg-black/5">
+  <motion.nav
+    variants={navContainer}
+    initial="hidden"
+    animate="show"
+    className="fixed bottom-8 left-1/2 translate-x-[-50%] z-20"
+  >
+    <motion.ul
+      variants={navList}
+      initial="hidden"
+      animate="show"
+      className="flex gap-2 flex-row justify-center items-center py-2 px-4 rounded-full backdrop-blur-[5px] bg-black/5"
+    >
       <NavItem text="Home" path="/" />
       <NavItem text="Projects" path="/projects" />
       <NavItem text="About" path="/about" />
       <div className="h-6 mx-3">
         <hr className="h-full border-l border-white/40" />
       </div>
-    </ul>
-  </nav>
+    </motion.ul>
+  </motion.nav>
 );
---
+++ src/components/Navigation/navigationVariants.ts
@@ -0,0 +1,26 @@
+export const navContainer = {
+  hidden: {
+    opacity: 0,
+    width: "0%",
+  },
+  show: {
+    opacity: 1,
+    width: "auto",
+    transition: {
+      duration: 0.25,
+      delay: 0.4,
+    },
+  },
+};
+
+export const navList = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      delay: 0.2,
+    },
+  },
+};
