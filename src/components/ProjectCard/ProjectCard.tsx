c1efb5fe80d1b96c13d7e6e3cb57b81deab97944
Leandro Argentiero <argentiero.leo@gmail.com>
2022-04-11 14:22:38 +0200

    Added re-usable card component

---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+declare interface ProjectCardProps {
+  href: string;
+  label: string;
+  title: string;
+  imageSrc: string;
+}
+
+export const ProjectCard = ({
+  href,
+  label,
+  title,
+  imageSrc,
+}: ProjectCardProps) => {
+  return (
+    <Link href={href} passHref>
+      <a className="group overflow-hidden rounded-2xl md:even:-translate-y-1/3">
+        <figure
+          role="listitem"
+          className="relative h-[15rem] w-full bg-red-200 lg:h-[20rem] xl:h-[25rem]"
+        >
+          <Image
+            className="object-cover transition duration-300 ease-in-out group-hover:scale-105"
+            src={imageSrc}
+            layout="fill"
+            alt="project-image"
+          />
+        </figure>
+        <div className="group-first: flex flex-col justify-center gap-2 bg-pastel-indigo px-6 py-6 font-display group-even:bg-pastel-pink md:py-12 lg:h-72 lg:gap-4 lg:px-12">
+          <span className="text-xs uppercase tracking-widest text-pastel-pink group-even:text-pastel-indigo lg:text-sm">
+            {label}
+          </span>
+          <h3 className="text-xl group-hover:underline lg:text-3xl">{title}</h3>
+        </div>
+      </a>
+    </Link>
+  );
+};
