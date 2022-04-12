e8285bd67b9d54d9a44ef6eae6bd1cb87f8e8b62
Leandro Argentiero <argentiero.leo@gmail.com>
2022-04-12 17:11:48 +0200

    Added image gallery

---
+++ src/components/ImageGallery/images.ts
@@ -0,0 +1,100 @@
+export const images = [
+  {
+    src: '/assets/indo-1.jpg',
+    width: 700, // landscape
+    height: 466,
+    location: 'Indonesia',
+    description: 'In between surf sessions',
+  },
+  {
+    src: '/assets/lanzarote-8.jpg',
+    width: 700, // portrait
+    height: 875,
+    location: 'Lanzarote',
+    description: 'On the edge of a volcanic crater',
+  },
+  {
+    src: '/assets/p29-nature-04.jpg',
+    width: 700, // landscape
+    height: 466,
+    location: 'Puglia',
+    description: 'Funghi quest with padre Ciccio',
+  },
+  {
+    src: '/assets/senegal-6.jpg',
+    width: 700, // landscape
+    height: 466,
+    location: 'Senegal',
+    description: 'Soaking up the winter sunbeams',
+  },
+  {
+    src: '/assets/lanzarote-10.jpg',
+    width: 700, // portrait
+    height: 875,
+    location: 'Lanzarote',
+    description: 'Local kid playing with a stick',
+  },
+  {
+    src: '/assets/senegal-3.jpg',
+    width: 700, // portrait
+    height: 875,
+    location: 'Senegal',
+    description: 'Inside the House of Slaves',
+  },
+  {
+    src: '/assets/portugal-5.jpg',
+    width: 700, // landscape
+    height: 466,
+    location: 'Algarve',
+    description: 'Wetsuits drying in the sun',
+  },
+  {
+    src: '/assets/lanzarote-6.jpg',
+    width: 700, // portrait
+    height: 875,
+    location: 'Lanzarote',
+    description: 'Dawn Patrol',
+  },
+  {
+    src: '/assets/senegal-4.jpg',
+    width: 700, // portrait
+    height: 875,
+    location: 'Senegal',
+    description: 'A goat in the courtyard',
+  },
+  {
+    src: '/assets/indonesia.jpg',
+    width: 700, // landscape
+    height: 466,
+    location: 'Indonesia',
+    description: 'Sunset views over Mount Agung',
+  },
+  {
+    src: '/assets/senegal-5.jpg',
+    width: 700, // landscape
+    height: 466,
+    location: 'Senegal',
+    description: 'Partie de foot',
+  },
+  {
+    src: '/assets/senegal.jpg',
+    width: 700, // portrait
+    height: 875,
+    location: 'Senegal',
+    description: 'Checking the incoming swell',
+  },
+  {
+    src: '/assets/senegal-9.jpg',
+    width: 700, // landscape
+    height: 466,
+    location: 'Senegal',
+    description: 'Soaking up the winter sunbeams',
+  },
+  {
+    src: '/assets/lanzarote-3.jpg',
+    width: 700, // portrait
+    height: 875,
+    location: 'Lanzarote',
+    description: 'Deserted building in the desert',
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,39 @@
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/sections/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/sections/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       animation: {
-        grain: "grain 1.5s steps(6) infinite",
+        grain: 'grain 1.5s steps(6) infinite',
+      },
+      backgroundImage: {
+        'gallery-image-gradient':
+          'linear-gradient(180deg,#00000057 0,rgba(0,0,0,.338) 3.5%,rgba(0,0,0,.324) 7%,rgba(0,0,0,.306) 10.35%,rgba(0,0,0,.285) 13.85%,rgba(0,0,0,.262) 17.35%,rgba(0,0,0,.237) 20.85%,rgba(0,0,0,.213) 24.35%,rgba(0,0,0,.188) 27.85%,rgba(0,0,0,.165) 31.35%,rgba(0,0,0,.144) 34.85%,rgba(0,0,0,.126) 38.35%,rgba(0,0,0,.112) 41.85%,rgba(0,0,0,.103) 45.35%,#0000001a 48.85%,rgba(0,0,0,.103) 52.35%,rgba(0,0,0,.112) 55.85%,rgba(0,0,0,.126) 59.35%,rgba(0,0,0,.144) 62.85%,rgba(0,0,0,.165) 66.35%,rgba(0,0,0,.188) 69.85%,rgba(0,0,0,.213) 73.35%,rgba(0,0,0,.237) 76.85%,rgba(0,0,0,.262) 80.35%,rgba(0,0,0,.285) 83.85%,rgba(0,0,0,.306) 87.35%,rgba(0,0,0,.324) 90.85%,rgba(0,0,0,.338) 94.35%,rgba(0,0,0,.347) 97.85%,#00000059)',
       },
       keyframes: {
         grain: {
-          "0%, 100%": { transform: "translate(0,0)" },
-          "10%": { transform: "translate(-5%,-10%)" },
-          "30%": { transform: "translate(3%,-15%)" },
-          "50%": { transform: "translate(12%,9%)" },
-          "70%": { transform: "translate(9%,4%)" },
-          "90%": { transform: "translate(-1%,7%)" },
+          '0%, 100%': { transform: 'translate(0,0)' },
+          '10%': { transform: 'translate(-5%,-10%)' },
+          '30%': { transform: 'translate(3%,-15%)' },
+          '50%': { transform: 'translate(12%,9%)' },
+          '70%': { transform: 'translate(9%,4%)' },
+          '90%': { transform: 'translate(-1%,7%)' },
         },
       },
       fontFamily: {
-        display: ["Space Grotesk", "serif"],
-        body: ["IBM Plex Sans", "sans-serif"],
+        display: ['Space Grotesk', 'serif'],
+        body: ['IBM Plex Sans', 'sans-serif'],
       },
       colors: {
-        "pastel-pink": "#F2E3EE",
-        "pastel-indigo": "#9FB0EA",
-        "pastel-blue": "#B0CDEB",
+        'pastel-pink': '#F2E3EE',
+        'pastel-indigo': '#9FB0EA',
+        'pastel-blue': '#B0CDEB',
       },
       container: {
         center: true,
-      },
-      gridTemplateColumns: {
-        24: "repeat(24, minmax(0, 1fr))",
       },
     },
   },
