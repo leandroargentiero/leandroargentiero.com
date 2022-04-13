bbd0bff908084de9f48f6a7113d8478ca4bb94c7
Leandro Argentiero <argentiero.leo@gmail.com>
2022-04-13 13:29:11 +0200

    Retouching Footer styles

---
+++ src/components/Footer/FooterCta.tsx
@@ -0,0 +1,20 @@
+export const FooterCta = () => (
+  <div className="flex flex-col">
+    <div className="mb-6">
+      <h3 className="mb-3 font-display text-2xl text-gray-900 lg:text-2xl xl:text-3xl">
+        Let&apos;s build something together.
+      </h3>
+      <p className="font-body text-gray-400">
+        Now that you know me, let&apos;s talk about you. <br />
+        Feel free to reach out if you&apos;ve a question, or just want to
+        connect.
+      </p>
+    </div>
+    <a
+      href="#"
+      className="font-display text-xl underline hover:no-underline lg:text-xl"
+    >
+      ciao@leandroargentiero.com
+    </a>
+  </div>
+);
