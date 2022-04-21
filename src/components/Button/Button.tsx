8499036695fdab351420f75e2117b2ff9bdcf484
Leandro Argentiero <argentiero.leo@gmail.com>
2022-04-21 09:54:17 +0200

    Added button animation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { FiChevronRight } from 'react-icons/fi';
 
 declare interface ButtonProps {
   children: string;
@@ -19,12 +20,13 @@ export const Button = ({
       <button
         type="button"
         className={classNames(
-          'rounded-full bg-gray-900 py-4 px-6 font-display text-base text-white outline-8 outline-offset-2 outline-gray-300 hover:cursor-pointer hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-900 hover:dark:bg-gray-300',
+          'group inline-flex items-center rounded-full bg-gray-900 py-4 px-6 font-display text-base text-white outline-8 outline-offset-2 outline-gray-300 transition-all duration-200 ease-in-out hover:cursor-pointer hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-900 hover:dark:bg-gray-300',
           { 'block w-full': isFullWidth }
         )}
         {...props}
       >
         {children}
+        <FiChevronRight className="ml-0 w-0 opacity-0 transition-all duration-200 ease-in-out group-hover:ml-2 group-hover:w-auto group-hover:opacity-100" />
       </button>
     </a>
   );
