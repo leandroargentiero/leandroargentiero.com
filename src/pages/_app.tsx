dcc1daa9b2fb85eb8e2550eb994ae905d0de18d6
Leandro Argentiero <argentiero.leo@gmail.com>
2022-04-25 10:15:52 +0200

    Added tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,48 @@
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
 import { DefaultSeo } from 'next-seo';
 import { ThemeProvider } from 'next-themes';
 
 import '../styles/globals.css';
+import * as gtag from '../lib/gtag';
 import SEO from '../../next-seo.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider enableSystem={true} attribute="class">
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </ThemeProvider>
